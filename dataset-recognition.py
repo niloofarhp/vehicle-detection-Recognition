27a97c3cc9e2d4f6211526f7ad94661dd0899fb7
niloufar <niloofar.hp@gmail.com>
2021-07-29 22:18:29 +0430

    installing requitements

--- dataset-recognition.py
+++ dataset-recognition.py
@@ -1,3 +1,7 @@
-import pytorch
+import tensorflow as tf
+def makeDatasetFromPaltes():
+  print("loading the images")
+
 def main():
   print("start making dataset by croping a big margin from located plates.")
+  makeDatasetFromPaltes()
